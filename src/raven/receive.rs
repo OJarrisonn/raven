418fed5f262523d2d7cccf4f4a2ae1f79c5c7f33
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-07-26 10:28:56 -0300

    refactor: extract message and file receiving into separate functions

--- src/raven/receive.rs
+++ src/raven/receive.rs
@@ -33,38 +33,38 @@ pub fn receive(mut stream: TcpStream, config: Arc<Config>) -> Result<()> {
         }
     };
 
-    let mut mailbox = MailBox::open(&config).context("Opening the mailbox")?; // Opens the mailbox to save the received messages
-
     match rv {
-        Raven::Text { text } => {
-            mailbox.add_message(sender, chrono::Utc::now(), text);
-            mailbox.save(&config)?;
-        }
-        Raven::File { name, content } => {
-            // Gets the folder where the files will be stored and ensures that it exists
-            let raven_arrivals = format!("{}/data", &config.raven_home);
-            if let Err(e) = util::ensure_folder(&raven_arrivals)
-                .context("Failed to create the folder to store files")
-            {
-                bail!("{}", e);
-            }
+        Raven::Text { text } => message(&config, sender, text),
+        Raven::File { name, content } => file(&config, sender, name, content)
+    }
+}
 
-            // Gets a non colliding filename
-            let path = format!("{}/{}", raven_arrivals, name);
-            let path = util::non_colliding_filename(&path);
+fn message(config: &Config, sender: String, text: String) -> Result<()> {
+    let mut mailbox = MailBox::open(&config).context("Opening the mailbox")?; // Opens the mailbox to save the received messages
+    mailbox.add_message(sender, chrono::Utc::now(), text);
+    mailbox.save(&config)?;
+    Ok(())
+}
 
-            // Writes the file to the disk
-            if let Err(e) = std::fs::write(&path, content).context("Saving the received file") {
-                bail!("Failed to write the file: {}", e);
-            }
-
-            mailbox.add_file(sender, chrono::Utc::now(), path);
-            mailbox
-                .save(&config)
-                .context("Saving the mailbox with the new received data")?;
-        
-        }
+fn file(config: &Config, sender: String, name: String, content: Vec<u8>) -> Result<()> {
+    // Gets the folder where the files will be stored and ensures that it exists
+    let raven_arrivals = format!("{}/data", &config.raven_home);
+    if let Err(e) = util::ensure_folder(&raven_arrivals).context("Failed to create the folder to store files") {
+        bail!("{}", e);
     }
 
+    // Gets a non colliding filename
+    let path = format!("{}/{}", raven_arrivals, name);
+    let path = util::non_colliding_filename(&path);
+
+    // Writes the file to the disk
+    if let Err(e) = std::fs::write(&path, content).context("Saving the received file") {
+        bail!("Failed to write the file: {}", e);
+    }
+
+    let mut mailbox = MailBox::open(&config).context("Opening the mailbox")?; // Opens the mailbox to save the received messages
+    mailbox.add_file(sender, chrono::Utc::now(), path);
+    mailbox.save(&config)?;
+    
     Ok(())
 }
